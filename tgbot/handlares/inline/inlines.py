030bb16144afd60bf5753fe45afdc22b82e9cf7e
Artur <artnyr2004@gmail.com>
2022-10-23 18:09:05 +0600

    finished inline bot

--- tgbot/handlares/inline/inlines.py
+++ tgbot/handlares/inline/inlines.py
@@ -14,7 +14,7 @@ async def inline_get_my_id(inline_query: types.InlineQuery):
     # await inline_query.answer(inline_query.from_user.id)
     text = inline_query.query or 'echo'
     link = "https://ru.wikipedia.org/wiki/"+text
-    input_content = types.InputTextMessageContent(link)
+    input_content = types.InputTextMessageContent(message_text=link)
     result_id: str = hashlib.md5(text.encode()).hexdigest()
     item = [types.InlineQueryResultArticle(
         id=result_id,
@@ -23,7 +23,3 @@ async def inline_get_my_id(inline_query: types.InlineQuery):
         url=link
     )]
     await inline_query.answer(item, cache_time=1, is_personal=True)
-    print(text)
-    # await inline_query.answer(results)
-    # don't forget to set cache_time=1 for testing (default is 300s or 5m)
-    await dp.bot.answer_inline_query(inline_query.id, results=[item])
